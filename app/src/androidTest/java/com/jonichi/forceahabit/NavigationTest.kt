c3becbdeb3f69a5c6eff76aa5350198a8bd8df6d
jonichi <jonichi16@gmail.com>
2025-01-12 10:46:19 +0800

    feat: add navigation with start destination home

---
+++ app/src/androidTest/java/com/jonichi/forceahabit/NavigationTest.kt
@@ -0,0 +1,34 @@
+package com.jonichi.forceahabit
+
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.navigation.compose.ComposeNavigator
+import androidx.navigation.testing.TestNavHostController
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class NavigationTest {
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+    private lateinit var navController: TestNavHostController
+
+    @Before
+    fun setupForceAHabitNavHost() {
+        composeTestRule.setContent {
+            navController =
+                TestNavHostController(LocalContext.current).apply {
+                    navigatorProvider.addNavigator(ComposeNavigator())
+                }
+            ForceAHabitApp(navController = navController)
+        }
+    }
+
+    @Test
+    fun fahNavHost_verifyHomeDestination() {
+        assertEquals(ForceAHabitScreen.Home.name, navController.currentBackStackEntry?.destination?.route)
+    }
+}
