4497098e8c1dd27430ad3b92bc5061b69aaadd98
jonichi <jonichi16@gmail.com>
2025-01-12 10:46:19 +0800

    feat: add navigation with start destination home

---
+++ app/src/main/java/com/jonichi/forceahabit/ForceAHabitApp.kt
@@ -0,0 +1,35 @@
+package com.jonichi.forceahabit
+
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun ForceAHabitApp(navController: NavHostController = rememberNavController()) {
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+        NavHost(
+            navController = navController,
+            startDestination = ForceAHabitScreen.Home.name,
+            modifier = Modifier.padding(innerPadding),
+        ) {
+            composable(
+                route = ForceAHabitScreen.Home.name,
+            ) {
+            }
+        }
+    }
+}
+
+enum class ForceAHabitScreen(
+    @StringRes val title: Int,
+) {
+    Home(title = R.string.app_name),
+}
