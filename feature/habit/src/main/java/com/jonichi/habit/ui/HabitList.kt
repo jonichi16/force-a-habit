85914d373c7153dad07d74d6809d1f7660190519
jonichi <jonichi16@gmail.com>
2025-01-12 16:14:20 +0800

    feat: add HabitList component

---
+++ feature/habit/src/main/java/com/jonichi/habit/ui/HabitList.kt
@@ -0,0 +1,13 @@
+package com.jonichi.habit.ui
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun HabitList(modifier: Modifier = Modifier) {
+    Text(
+        text = "Home",
+        modifier = modifier,
+    )
+}
