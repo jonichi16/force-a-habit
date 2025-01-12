6f77d1fba0e1a6372b93be07c2b9add92dcd9f7f
jonichi <jonichi16@gmail.com>
2025-01-12 18:28:26 +0800

    feat: add HabitListPreview and refactor gradle dependencies

--- feature/habit/src/main/java/com/jonichi/habit/ui/HabitList.kt
+++ feature/habit/src/main/java/com/jonichi/habit/ui/HabitList.kt
@@ -3,6 +3,8 @@ package com.jonichi.habit.ui
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.jonichi.uicommon.theme.ForceAHabitTheme
 
 @Composable
 fun HabitList(modifier: Modifier = Modifier) {
@@ -10,4 +12,12 @@ fun HabitList(modifier: Modifier = Modifier) {
         text = "Home",
         modifier = modifier,
     )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HabitListPreview() {
+    ForceAHabitTheme {
+        HabitList()
+    }
 }
