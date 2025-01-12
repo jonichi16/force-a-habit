22fe61a312f3ce17a0b1e4f7ea2d41d4e56fa22c
jonichi <jonichi16@gmail.com>
2025-01-12 18:14:19 +0800

    refactor: move theme to uicommon module

--- app/src/main/java/com/jonichi/forceahabit/MainActivity.kt
+++ app/src/main/java/com/jonichi/forceahabit/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.jonichi.forceahabit.ui.theme.ForceAHabitTheme
+import com.jonichi.uicommon.theme.ForceAHabitTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- lib/uicommon/src/main/java/com/jonichi/uicommon/theme/Type.kt
+++ lib/uicommon/src/main/java/com/jonichi/uicommon/theme/Type.kt
@@ -1,10 +1,10 @@
-package com.jonichi.forceahabit.ui.theme
+package com.jonichi.uicommon.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.googlefonts.Font
 import androidx.compose.ui.text.googlefonts.GoogleFont
-import com.jonichi.forceahabit.R
+import com.jonichi.uicommon.R
 
 val provider =
     GoogleFont.Provider(
