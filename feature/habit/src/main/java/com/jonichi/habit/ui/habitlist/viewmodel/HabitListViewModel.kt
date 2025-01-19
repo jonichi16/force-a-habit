cc0ee333560d963abf98ba9dc42762a783a3d4f1
jonichi <jonichi16@gmail.com>
2025-01-19 16:55:33 +0800

    feat: use viewmodel in the habitlist

--- feature/habit/src/main/java/com/jonichi/habit/ui/habitlist/viewmodel/HabitListViewModel.kt
+++ feature/habit/src/main/java/com/jonichi/habit/ui/habitlist/viewmodel/HabitListViewModel.kt
@@ -1,28 +1,36 @@
 package com.jonichi.habit.ui.habitlist.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.jonichi.habit.ui.habitlist.Habit
 import com.jonichi.habit.ui.habitlist.HabitUiState
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 
 class HabitListViewModel : ViewModel() {
     private val _uiState = MutableStateFlow<HabitUiState>(HabitUiState.Loading)
-    val uiState = _uiState.asStateFlow()
+    val uiState = _uiState
+        .onStart { loadHabits() }
+        .stateIn(
+            viewModelScope,
+            SharingStarted.WhileSubscribed(5000L),
+            HabitUiState.Loading
+        )
 
-    init {
-        loadHabit()
-    }
-
-    private fun loadHabit() {
-        _uiState.value =
-            HabitUiState.Success(
-                habits =
-                    listOf(
-                        Habit(title = "Habit 1"),
-                        Habit(title = "Habit 2"),
-                        Habit(title = "Habit 3"),
-                    ),
+    private fun loadHabits() {
+        viewModelScope.launch {
+            delay(1000) // Simulate network or database delay
+            _uiState.value = HabitUiState.Success(
+                habits = listOf(
+                    Habit(title = "Habit 1"),
+                    Habit(title = "Habit 2"),
+                    Habit(title = "Habit 3")
+                )
             )
+        }
     }
 }
