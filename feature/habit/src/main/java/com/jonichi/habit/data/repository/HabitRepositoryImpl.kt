a4aafe507fa35ca5126eb5cd4b79d12ffea5787c
jonichi <jonichi16@gmail.com>
2025-01-29 11:18:35 +0800

    add habit repository and implementation

---
+++ feature/habit/src/main/java/com/jonichi/habit/data/repository/HabitRepositoryImpl.kt
@@ -0,0 +1,23 @@
+package com.jonichi.habit.data.repository
+
+import com.jonichi.database.HabitDao
+import com.jonichi.habit.data.mapper.toDomain
+import com.jonichi.habit.data.mapper.toEntity
+import com.jonichi.habit.domain.model.Habit
+import com.jonichi.habit.domain.repository.HabitRepository
+import jakarta.inject.Inject
+
+class HabitRepositoryImpl
+    @Inject
+    constructor(
+        private val habitDao: HabitDao
+    ) : HabitRepository
+{
+    override suspend fun upsert(habit: Habit) {
+        habitDao.upsert(habit.toEntity())
+    }
+
+    override suspend fun getAllHabits(): List<Habit> {
+        return habitDao.getAllHabits().map { habitEntity -> habitEntity.toDomain() }
+    }
+}
