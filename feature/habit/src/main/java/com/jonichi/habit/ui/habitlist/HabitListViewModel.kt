238b70d7067bfc1e270483903e2e8f963f13a6b6
jonichi <jonichi16@gmail.com>
2025-01-29 10:29:13 +0800

    update Habit properties

--- feature/habit/src/main/java/com/jonichi/habit/ui/habitlist/HabitListViewModel.kt
+++ feature/habit/src/main/java/com/jonichi/habit/ui/habitlist/HabitListViewModel.kt
@@ -9,6 +9,7 @@ import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
+import java.time.LocalTime
 
 class HabitListViewModel : ViewModel() {
     companion object {
@@ -32,9 +33,24 @@ class HabitListViewModel : ViewModel() {
                 HabitListUiState.Success(
                     habits =
                         listOf(
-                            Habit(title = "Habit 1"),
-                            Habit(title = "Habit 2"),
-                            Habit(title = "Habit 3"),
+                            Habit(
+                                id = 1,
+                                title = "Habit 1",
+                                schedule = LocalTime.of(12, 0),
+                                isStrict = false,
+                            ),
+                            Habit(
+                                id = 2,
+                                title = "Habit 2",
+                                schedule = LocalTime.of(12, 0),
+                                isStrict = false,
+                            ),
+                            Habit(
+                                id = 3,
+                                title = "Habit 3",
+                                schedule = LocalTime.of(12, 0),
+                                isStrict = false,
+                            ),
                         ),
                 )
         }
