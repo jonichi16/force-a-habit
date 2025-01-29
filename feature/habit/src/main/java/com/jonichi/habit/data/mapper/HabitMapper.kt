8e6d7f6d35fed483e1263ff55dfd0c041e5526d1
jonichi <jonichi16@gmail.com>
2025-01-29 10:29:45 +0800

    add Habit mapper

---
+++ feature/habit/src/main/java/com/jonichi/habit/data/mapper/HabitMapper.kt
@@ -0,0 +1,26 @@
+package com.jonichi.habit.data.mapper
+
+import com.jonichi.database.model.HabitEntity
+import com.jonichi.habit.domain.model.Habit
+
+fun HabitEntity.toDomain(): Habit {
+    return Habit(
+        id = id,
+        title = title,
+        schedule = schedule,
+        isStrict = isStrict,
+        createdAt = createdAt,
+        updatedAt = updatedAt,
+    )
+}
+
+fun Habit.toEntity(): HabitEntity {
+    return HabitEntity(
+        id = id,
+        title = title,
+        schedule = schedule,
+        isStrict = isStrict,
+        createdAt = createdAt,
+        updatedAt = updatedAt,
+    )
+}
