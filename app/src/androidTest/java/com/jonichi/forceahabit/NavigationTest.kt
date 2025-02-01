c84696a08d8542c53d2028f1acc550ae2902fc6b
jonichi <jonichi16@gmail.com>
2025-02-01 10:50:51 +0800

    refactor navigation test to add hilt

--- app/src/androidTest/java/com/jonichi/forceahabit/NavigationTest.kt
+++ app/src/androidTest/java/com/jonichi/forceahabit/NavigationTest.kt
@@ -1,46 +1,38 @@
 package com.jonichi.forceahabit
 
-import androidx.activity.ComponentActivity
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithText
-import androidx.navigation.compose.ComposeNavigator
-import androidx.navigation.testing.TestNavHostController
-import com.jonichi.uicommon.theme.ForceAHabitTheme
+import com.jonichi.habit.di.RepositoryModule
+import com.jonichi.habit.domain.repository.HabitRepository
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
-import org.junit.Assert.assertEquals
+import dagger.hilt.android.testing.UninstallModules
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import javax.inject.Inject
 
+@UninstallModules(RepositoryModule::class)
 @HiltAndroidTest
 class NavigationTest {
-    @get:Rule(order = 1)
+    @get:Rule(order = 0)
     var hiltRule = HiltAndroidRule(this)
 
-    @get:Rule(order = 2)
-    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+    @get:Rule(order = 1)
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
 
-    private lateinit var navController: TestNavHostController
+    @Inject
+    lateinit var repository: HabitRepository
 
     @Before
     fun setupForceAHabitNavHost() {
-        composeTestRule.setContent {
-            ForceAHabitTheme {
-                navController =
-                    TestNavHostController(LocalContext.current).apply {
-                        navigatorProvider.addNavigator(ComposeNavigator())
-                    }
-                ForceAHabitApp(navController = navController)
-            }
-        }
+        hiltRule.inject()
     }
 
     @Test
     fun fahNavHost_verifyHomeDestination() {
-        assertEquals(ForceAHabitScreen.Home.name, navController.currentBackStackEntry?.destination?.route)
         composeTestRule.onNodeWithText("Home").assertIsDisplayed()
+        composeTestRule.onNodeWithText("Habit 1").assertIsDisplayed()
     }
 }
