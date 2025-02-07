d7ee66f989f0419591768361287c5d1c2bbca749
jonichi <jonichi16@gmail.com>
2025-02-07 17:13:47 +0800

    move home screen test to app

--- app/src/androidTest/java/com/jonichi/forceahabit/ui/HomeScreenTest.kt
+++ app/src/androidTest/java/com/jonichi/forceahabit/ui/HomeScreenTest.kt
@@ -1,4 +1,4 @@
-package com.jonichi.habit.ui.habitlist
+package com.jonichi.forceahabit.ui
 
 import androidx.compose.runtime.remember
 import androidx.compose.ui.test.assertCountEquals
@@ -9,6 +9,8 @@ import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import com.jonichi.common.constant.TAG_ADD_HABIT_NAVIGATION
 import com.jonichi.habit.domain.model.Habit
+import com.jonichi.habit.ui.habitlist.HabitList
+import com.jonichi.habit.ui.habitlist.HabitListUiState
 import com.jonichi.uicommon.theme.ForceAHabitTheme
 import org.junit.Rule
 import org.junit.Test
@@ -25,26 +27,26 @@ class HomeScreenTest {
                 remember {
                     HabitListUiState.Success(
                         habits =
-                            listOf(
-                                Habit(
-                                    id = 1,
-                                    title = "Habit 1",
-                                    schedule = LocalTime.of(12, 0),
-                                    isStrict = false,
-                                ),
-                                Habit(
-                                    id = 2,
-                                    title = "Habit 2",
-                                    schedule = LocalTime.of(14, 0),
-                                    isStrict = false,
-                                ),
-                                Habit(
-                                    id = 3,
-                                    title = "Habit 3",
-                                    schedule = LocalTime.of(12, 0),
-                                    isStrict = false,
-                                ),
+                        listOf(
+                            Habit(
+                                id = 1,
+                                title = "Habit 1",
+                                schedule = LocalTime.of(12, 0),
+                                isStrict = false,
                             ),
+                            Habit(
+                                id = 2,
+                                title = "Habit 2",
+                                schedule = LocalTime.of(14, 0),
+                                isStrict = false,
+                            ),
+                            Habit(
+                                id = 3,
+                                title = "Habit 3",
+                                schedule = LocalTime.of(12, 0),
+                                isStrict = false,
+                            ),
+                        ),
                     )
                 }
 
