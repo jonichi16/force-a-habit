559e3c6d72d95167ca201b2a58a3c4c66f29c232
jonichi <jonichi16@gmail.com>
2025-02-07 21:40:41 +0800

    add HabitFormViewModel

---
+++ feature/habit/src/test/java/com/jonichi/habit/ui/habitform/HabitFormViewModelTest.kt
@@ -0,0 +1,90 @@
+package com.jonichi.habit.ui.habitform
+
+import junit.framework.TestCase.assertEquals
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import java.time.LocalTime
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class HabitFormViewModelTest {
+    private lateinit var viewModel: HabitFormViewModel
+    private val testDispatcher = StandardTestDispatcher()
+
+    @Before
+    fun setUp() {
+        Dispatchers.setMain(testDispatcher)
+        viewModel = HabitFormViewModel()
+    }
+
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain()
+    }
+
+    @Test
+    fun `uiState should be Loading then Success`() =
+        runTest {
+            val job =
+                launch {
+                    viewModel.uiState.collect {}
+                }
+
+            val initialState = viewModel.uiState.first()
+
+            assertEquals(HabitFormUiState.Loading, initialState)
+
+            advanceUntilIdle()
+
+            val successState = viewModel.uiState.first() as HabitFormUiState.Success
+            assertEquals(successState.title, "")
+            assertEquals(successState.schedule, LocalTime.of(8, 0))
+
+            job.cancel()
+        }
+
+    @Test
+    fun `viewModel should be able to update title`() =
+        runTest {
+            val job =
+                launch {
+                    viewModel.uiState.collect {}
+                }
+
+            advanceUntilIdle()
+            viewModel.updateTitle("Habit 1")
+            advanceUntilIdle()
+
+            val updatedState = viewModel.uiState.first() as HabitFormUiState.Success
+            assertEquals("Habit 1", updatedState.title)
+
+            job.cancel()
+        }
+
+    @Test
+    fun `viewModel should be able to update schedule`() =
+        runTest {
+            val job =
+                launch {
+                    viewModel.uiState.collect {}
+                }
+
+            advanceUntilIdle()
+            viewModel.updateSchedule(LocalTime.of(3, 30))
+            advanceUntilIdle()
+
+            val updatedState = viewModel.uiState.first() as HabitFormUiState.Success
+            assertEquals(LocalTime.of(3, 30), updatedState.schedule)
+
+            job.cancel()
+        }
+}
