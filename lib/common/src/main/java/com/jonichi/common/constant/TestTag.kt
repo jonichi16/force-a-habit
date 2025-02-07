8f783f21a83021105496f2e94999d1ba59ab24db
jonichi <jonichi16@gmail.com>
2025-02-07 14:58:53 +0800

    rename TAG_ADD_HABIT to TAG_ADD_HABIT_NAVIGATION

--- lib/common/src/main/java/com/jonichi/common/constant/TestTag.kt
+++ lib/common/src/main/java/com/jonichi/common/constant/TestTag.kt
@@ -1,4 +1,4 @@
 package com.jonichi.common.constant
 
-const val TAG_ADD_HABIT = "TAG_ADD_HABIT"
+const val TAG_ADD_HABIT_NAVIGATION = "TAG_ADD_HABIT_NAVIGATION"
 const val TAG_BACK_ARROW = "TAG_BACK_ARROW"
