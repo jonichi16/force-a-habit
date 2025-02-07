1e0e05aace198354efbbc86ae8fa0b2acc3d457e
jonichi <jonichi16@gmail.com>
2025-02-07 18:51:08 +0800

    add title and time fields to HabitForm

--- feature/habit/src/main/java/com/jonichi/habit/ui/habitform/HabitForm.kt
+++ feature/habit/src/main/java/com/jonichi/habit/ui/habitform/HabitForm.kt
@@ -1,18 +1,66 @@
 package com.jonichi.habit.ui.habitform
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import com.jonichi.common.constant.TAG_FORM_INPUT_FIELD
 import com.jonichi.uicommon.components.HabitTopAppBar
+import com.jonichi.uicommon.theme.ForceAHabitTheme
+import java.time.LocalTime
 
 @Composable
 fun HabitForm(
     onBackAction: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
+
     Column(modifier = modifier) {
         HabitTopAppBar(title = "Add Habit", onBackAction = onBackAction)
-        Text(text = "Add Habit")
+        HabitTextField(
+            label = "Title",
+            value = "",
+            onValueChange = {},
+        )
+        HabitTextField(
+            label = "Time",
+            value = LocalTime.of(8, 0).toString(),
+            readOnly = true,
+            onValueChange = {},
+            modifier =
+                Modifier
+                    .clickable {
+                        println("")
+                    }
+        )
+    }
+}
+
+@Composable
+fun HabitTextField(
+    label: String,
+    value: String,
+    onValueChange: (String) -> Unit,
+    readOnly: Boolean = false,
+    modifier: Modifier = Modifier
+) {
+    TextField(
+        label = { Text(text = label) },
+        value = value,
+        onValueChange = onValueChange,
+        readOnly = readOnly,
+        modifier = modifier.testTag(TAG_FORM_INPUT_FIELD)
+    )
+}
+
+@PreviewLightDark
+@Composable
+fun HabitFormPreview() {
+    ForceAHabitTheme {
+        HabitForm(onBackAction = {})
     }
 }
--- lib/common/src/main/java/com/jonichi/common/constant/TestTag.kt
+++ lib/common/src/main/java/com/jonichi/common/constant/TestTag.kt
@@ -2,3 +2,4 @@ package com.jonichi.common.constant
 
 const val TAG_ADD_HABIT_NAVIGATION = "TAG_ADD_HABIT_NAVIGATION"
 const val TAG_BACK_ARROW = "TAG_BACK_ARROW"
+const val TAG_FORM_INPUT_FIELD = "TAG_FORM_INPUT_FIELD"
