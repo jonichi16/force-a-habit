2d06b7bfc2d12a10a7c9291c0f272ff88b629071
jonichi <jonichi16@gmail.com>
2025-02-08 16:58:31 +0800

    resolve lint issue

--- feature/habit/src/main/java/com/jonichi/habit/ui/habitform/HabitFormEvent.kt
+++ feature/habit/src/main/java/com/jonichi/habit/ui/habitform/HabitFormEvent.kt
@@ -4,7 +4,10 @@ import java.time.LocalTime
 
 sealed interface HabitFormEvent {
     data class UpdateTitle(val title: String) : HabitFormEvent
-    data class UpdateSchedule(val schedule: LocalTime): HabitFormEvent
+
+    data class UpdateSchedule(val schedule: LocalTime) : HabitFormEvent
+
     data object ToggleTimeDialog : HabitFormEvent
+
     data object SaveHabit : HabitFormEvent
 }
