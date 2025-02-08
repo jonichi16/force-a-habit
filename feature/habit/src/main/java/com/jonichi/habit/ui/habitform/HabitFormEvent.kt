79b5857261004f89663a4bf1eaf1d9deb944abb7
jonichi <jonichi16@gmail.com>
2025-02-08 15:59:22 +0800

    refactor HabitFormViewModel onEvent

---
+++ feature/habit/src/main/java/com/jonichi/habit/ui/habitform/HabitFormEvent.kt
@@ -0,0 +1,10 @@
+package com.jonichi.habit.ui.habitform
+
+import java.time.LocalTime
+
+sealed interface HabitFormEvent {
+    data class UpdateTitle(val title: String) : HabitFormEvent
+    data class UpdateSchedule(val schedule: LocalTime): HabitFormEvent
+    data object ToggleTimeDialog : HabitFormEvent
+    data object SaveHabit : HabitFormEvent
+}
