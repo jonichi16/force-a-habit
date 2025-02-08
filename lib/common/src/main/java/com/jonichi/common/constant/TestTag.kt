7960fe2c05d0a170bd23c6d9638e1299283f964e
jonichi <jonichi16@gmail.com>
2025-02-08 09:17:00 +0800

    add functionality for toggling time dialog

--- lib/common/src/main/java/com/jonichi/common/constant/TestTag.kt
+++ lib/common/src/main/java/com/jonichi/common/constant/TestTag.kt
@@ -3,3 +3,4 @@ package com.jonichi.common.constant
 const val TAG_ADD_HABIT_NAVIGATION = "TAG_ADD_HABIT_NAVIGATION"
 const val TAG_BACK_ARROW = "TAG_BACK_ARROW"
 const val TAG_FORM_INPUT_FIELD = "TAG_FORM_INPUT_FIELD"
+const val TAG_TIME_DIALOG = "TAG_TIME_DIALOG"
