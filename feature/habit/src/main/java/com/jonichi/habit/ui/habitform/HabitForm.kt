08a1d35fe6eb3935d848d25c345de7074ea4abce
jonichi <jonichi16@gmail.com>
2025-02-08 10:31:05 +0800

    add TimeInput

--- feature/habit/src/main/java/com/jonichi/habit/ui/habitform/HabitForm.kt
+++ feature/habit/src/main/java/com/jonichi/habit/ui/habitform/HabitForm.kt
@@ -2,11 +2,17 @@ package com.jonichi.habit.ui.habitform
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldColors
 import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.TimeInput
+import androidx.compose.material3.TimePicker
+import androidx.compose.material3.rememberTimePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -15,6 +21,8 @@ import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import com.jonichi.common.constant.DEFAULT_HOUR
 import com.jonichi.common.constant.DEFAULT_MINUTE
+import com.jonichi.common.constant.TAG_BUTTON_CANCEL
+import com.jonichi.common.constant.TAG_BUTTON_CONFIRM
 import com.jonichi.common.constant.TAG_FORM_INPUT_FIELD
 import com.jonichi.common.constant.TAG_TIME_DIALOG
 import com.jonichi.uicommon.components.HabitTopAppBar
@@ -62,7 +70,13 @@ fun HabitForm(
                             }
                 )
                 if (state.isTimeDialogOpen) {
-                    Text(text = "Time Dialog", modifier=Modifier.testTag(TAG_TIME_DIALOG))
+                    TimePickerDialog(
+                        onToggleTimeDialog = onToggleTimeDialog,
+                        confirmButton = { hour, minute ->
+                            onUpdateSchedule(LocalTime.of(hour, minute))
+                            onToggleTimeDialog()
+                                        },
+                    )
                 }
             }
         }
@@ -90,12 +104,49 @@ fun HabitTextField(
     )
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TimePickerDialog(
+    onToggleTimeDialog: () -> Unit,
+    confirmButton: (hour: Int, minute: Int) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val timePickerState = rememberTimePickerState(
+        initialHour = DEFAULT_HOUR,
+        initialMinute = DEFAULT_MINUTE
+    )
+    AlertDialog(
+        onDismissRequest = onToggleTimeDialog,
+        confirmButton = {
+            TextButton(
+                onClick = {
+                    confirmButton(timePickerState.hour, timePickerState.minute)
+                          },
+                modifier = Modifier.testTag(TAG_BUTTON_CONFIRM)) {
+                Text(text = "Confirm")
+            }
+        },
+        dismissButton = {
+            TextButton(onClick = onToggleTimeDialog, modifier = Modifier.testTag(TAG_BUTTON_CANCEL)) {
+                Text(text = "Cancel")
+            }
+        },
+        title = { Text("Select time") },
+        text = {
+            TimeInput(
+                state = timePickerState,
+                modifier = modifier.testTag(TAG_TIME_DIALOG)
+            )
+        }
+    )
+}
+
 @PreviewLightDark
 @Composable
 fun HabitFormPreview() {
     ForceAHabitTheme {
         HabitForm(
-            state = HabitFormUiState.Success(),
+            state = HabitFormUiState.Success(isTimeDialogOpen = true),
             onUpdateTitle = {},
             onBackAction = {},
             onUpdateSchedule = {},
