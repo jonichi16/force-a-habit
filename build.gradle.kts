dfae40176229bb0dea5a0d4c72781365cb31d7f3
jonichi <jonichi16@gmail.com>
2025-02-18 19:54:48 +0800

    add workflow for deployment

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,4 +6,5 @@ plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.hiltDagger) apply false
     alias(libs.plugins.devtoolsKsp) apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
 }
